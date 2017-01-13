40d1c6c8f8f0302448551c68594d7fafba8bb81d
Aralya Phinith <aphinith84@gmail.com>
2017-01-13 11:02:49 -0800

    searchbar now populates results from viki api, move search bar to flush left along with results, results clear when no input value

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
+
 $(document).ready(function() {
   $(".search-bar").keyup(function(e) {
     var inputValue = $(".search-bar-input").val().toString();
-    console.log('inputValue: ', inputValue);
-    $(".autopopulated-results ul").empty().append("<li>"+inputValue+"</li>");
+
+    console.log('this is inputValue: ', inputValue);
+    
+    if (!inputValue) {
+      $(".autopopulated-results").empty().css({"border": "0"});
+    };
+
+    function appendTitles(titles) {
+      // console.log('this are the new titles: ', titles);
+      $(".autopopulated-results").empty().css({"border": "1px solid black"});
+      for (var i = 0; i < titles.length; i++){
+        var title = titles[i];
+        console.log('this is title: ', title );
+        $(".autopopulated-results").append("<div>"+title+"</div class='testing'>")
+      }
+    };
+
+    $.get('https://api.viki.io/v4/search.json?c='+inputValue+'&per_page=5&with_people=true&app=100266a&t=1440586215', function(data) {
+      var titles = []
+      for (var i = 0; i < 5; i++) {
+        var title = data[i].tt;
+        titles.push(title);
+      };
+      appendTitles(titles);
+    });
   })
 })
+
+// https://api.viki.io/v4/search.json?c=boys&per_page=5&with_people=true&app=100266a&t=1440586215
